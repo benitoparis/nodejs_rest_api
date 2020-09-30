100dd595955dd90aebdd55b4a93e0addafb0ec8b
benoit <benoit.gallou0gmail.com>
2020-09-30 22:18:30 +0200

    gestion des routes dun controller

--- controllers/players.js
+++ controllers/players.js
@@ -17,6 +17,18 @@ class playerController {
       res.send('les joueurs');
     });
 
+    this.router.get('/players/add_new', (req, res)=> {
+      console.log('url players');
+      res.send('<form method="post" action="/players/create"><label>Nom</label><input type="text" name="nom" /><button type="submit">ok</button></form>');
+    });
+
+
+    this.router.post('/players/create', (req, res)=> {
+      console.log('req', req);
+      console.log('post players');
+      res.redirect('/hello');
+    });
+
     //this.router.get('/players', this.getPlayers.bind(this));
     // this.router.get();
     // this.router.post();
