6a4879bddfe01da1c9080be152308ec4d1f680f1
benoit <benoit.gallou0gmail.com>
2020-09-30 22:06:38 +0200

    maj de lapi rest avec nodejs

--- controllers/players.js
+++ controllers/players.js
@@ -1,18 +1,23 @@
 // On importe la clas
-var playerService = require('../services/players');
+//const playerService = require('../services/players');
 
 // Classe de la ressource joueur
-class playersController {
+class playerController {
 
   // A l'instanciation
   constructor(router){
     this.router = router;
-
   }
 
   // Méthode pour déclarer les routes pour la ressource player
   registerRoutes(){
-    this.router.get('/players', this.getPlayers.bind(this));
+    console.log('registerRoutes');
+    this.router.get('/players', (req, res)=> {
+      console.log('url players');
+      res.send('les joueurs');
+    });
+
+    //this.router.get('/players', this.getPlayers.bind(this));
     // this.router.get();
     // this.router.post();
     // this.router.put();
@@ -20,6 +25,8 @@ class playersController {
 
   // Méthode pour récupérer les joueurs
   getPlayers(){
-    var players = playerService.getPlayers();
+    //var players = playerService.getPlayers();
   }
 }
+
+module.exports = playerController;
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 
 var express = require('express');
 app = express();
+const router = express.Router();
+let playerController = require('./controllers/players');
+
+new playerController(router).registerRoutes();
+
 app.route('/hello').get(function(req, res) {
+    console.log('hello');
     res.send('hello world !');
 });
 
-
+app.use('/', router);
 app.use(express.static('public'));
 
 app.listen(8080);
+
+console.log('dddd');
