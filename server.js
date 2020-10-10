676ac809bb89394757f347b09118bd5b441891e8
benoit <benoit.gallou0gmail.com>
2020-10-10 17:10:07 +0200

    add bootstrap in project

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 
 var express = require('express');
+const path = require('path');
 app = express();
+let ejs = require('ejs');
+let playersList = ['geddy', 'neil', 'alex'];
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
 const router = express.Router();
+
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+
 let playerController = require('./controllers/players');
 
 new playerController(router).registerRoutes();
 
 app.route('/hello').get(function(req, res) {
     console.log('hello');
-    res.send('hello world !');
+    //res.send('hello world !');
+    res.render('players', {list: playersList, title: 'Les joueurs'})
 });
 
 app.use('/', router);
